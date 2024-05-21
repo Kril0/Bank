b86699f759f2e079e607e19d336f5426f4e0d19d
Logan Krieg <logank72@gmail.com>
2024-05-21 00:33:34 -0400

    added code to functions

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
     }
+static Scanner scnr = new Scanner(System.in);
+    static double balance = 0.00;
+    public static void bank(){
+        double totalExpenses = 0.00;
+        double balance = 0.00;
+    }
 
-    public static double balance(){
+    public static double getBalance(double balance){
+        return balance;
+    }
 
+    public static void deposit(){
+        System.out.println("Enter amount to deposit");
+        double userDeposit = 0.00;
+        userDeposit = scnr.nextDouble();
+        balance += userDeposit;
+    }
+
+    public static void withdraw(){
+        System.out.println("Enter amount to withdraw");
+        double userWithdraw = 0.00;
+        userWithdraw = scnr.nextDouble();
+        balance -= userWithdraw;
 
     }
 
-    public static double deposit(){
-
-    }
-
-    public static double withdraw(){
-
-
-    }
 
 
 
