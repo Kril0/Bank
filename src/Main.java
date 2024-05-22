b107bfdd0abdfac23ca6ac513250523d18185d55
Logan Krieg <logank72@gmail.com>
2024-05-22 19:36:52 -0400

    added budget and expense, need more work

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import static java.lang.System.*;
-
+import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args){
@@ -8,7 +8,9 @@ public class Main {
         out.println("1.    Balance");
         out.println("2.    Withdraw");
         out.println("3.    Deposit");
-        out.println("4.    Quit");
+        out.println("4.    Budget");
+        out.println("5.    Expenses");
+        out.println("6.    Quit");
         int userInput = scnr.nextInt();
 
             if (userInput == 1){
@@ -19,6 +21,16 @@ public class Main {
             }
             else if (userInput == 3){
                 deposit();
+            }
+            else if (userInput == 4){
+                budget();
+            }
+            else if (userInput == 5){
+                expenses();
+            }
+            else if (userInput == 6){
+                out.println("Goodbye");
+                System. exit(0);
             }
             else{
                 out.println("Error: Invalid input");
@@ -37,13 +49,12 @@ static Scanner scnr = new Scanner(in);
         return balance;
     }
 
-    //TODO: Make value have $ and 2 decimal
     public static void deposit(){
         out.println("Enter amount to deposit");
         double userDeposit = 0.00;
         userDeposit = scnr.nextDouble();
         balance += userDeposit;
-        out.println("Your new balance is " + balance);
+        out.printf("Your new balance is $%.2f", balance);
 
     }
 
@@ -55,7 +66,7 @@ static Scanner scnr = new Scanner(in);
         userWithdraw = scnr.nextDouble();
         if (balance - userWithdraw > 0.00){
             balance -= userWithdraw;
-            out.println("Your new balance is " + balance);
+            out.printf("Your new balance is $%.2f", balance);
         }
         else if (balance - userWithdraw < 0.00){
              out.println("Negative balance, can not withdraw");
@@ -64,6 +75,27 @@ static Scanner scnr = new Scanner(in);
             out.println("Error: withdraw()");
         }
 
+    }
+
+    public static void budget(){
+        out.println("What is your maximum budget?");
+        double maxBudget = scnr.nextDouble();
+    }
+
+    public static void expensemap(String r, Double e){
+        HashMap<String, Double> expense = new HashMap<String, Double>();
+        expense.put(r , e);
+
+    }
+    //TODO: HashMap ends program
+    public static void expenses(){
+       Double userExpense = 0.00;
+       String expenseReason;
+       out.println("How much is the expense?");
+       userExpense = scnr.nextDouble();
+       out.println("What is the reason for the expense?");
+       expenseReason = scnr.nextLine();
+       expensemap(expenseReason, userExpense);
     }
 
 
